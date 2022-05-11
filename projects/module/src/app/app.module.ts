438f67dab51a664bae56d3dc8c4a959042c82942
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-11 14:53:49 +0200

    material

--- projects/module/src/app/app.module.ts
+++ projects/module/src/app/app.module.ts
@@ -3,6 +3,9 @@ import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from './app.component';
 import {IlpnComponentsModule} from 'ilpn-components';
 import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatSlideToggleModule} from '@angular/material/slide-toggle';
+import {ReactiveFormsModule} from '@angular/forms';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 @NgModule({
     declarations: [
@@ -12,6 +15,9 @@ import {FlexLayoutModule} from '@angular/flex-layout';
         BrowserModule,
         IlpnComponentsModule,
         FlexLayoutModule,
+        ReactiveFormsModule,
+        MatSlideToggleModule,
+        BrowserAnimationsModule,
     ],
     providers: [],
     bootstrap: [AppComponent]
