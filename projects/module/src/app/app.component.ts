17c87b84532e6be6bc3437bd9ed1f09376dfa855
Jakub Kovář <6195502+minop@users.noreply.github.com>
2022-05-11 13:29:11 +0200

    file upload with FD support

--- projects/module/src/app/app.component.ts
+++ projects/module/src/app/app.component.ts
@@ -1,14 +1,16 @@
 import {Component} from '@angular/core';
 import {APP_BASE_HREF} from '@angular/common';
+import {FD_PETRI_NET} from 'ilpn-components';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
     providers: [
-        // TODO base href
-        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/'}
+        {provide: APP_BASE_HREF, useValue: '/ilovepetrinets/horse/'}
     ]
 })
 export class AppComponent {
+
+    readonly FD_PN = FD_PETRI_NET;
 }
